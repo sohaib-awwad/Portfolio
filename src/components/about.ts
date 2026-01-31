73b6f90d03fbb9b7a257f2c6f937b5cbaf55937e
sohaib-awwad <sohaibawwad8@gmail.com>
2026-01-31 21:28:58 +0300

    test

--- src/components/about.ts
+++ src/components/about.ts
@@ -3,7 +3,7 @@ export function createAboutSection(): HTMLElement {
   const el = document.createElement("section");
   el.className = "container about";
   el.id = "about";
-  el.setAttribute("aria-labelledby", "about-title");//test
+  el.setAttribute("aria-labelledby", "about-title");
   el.innerHTML = `
     <header class="about__header"
       data-aos="fade-up"
